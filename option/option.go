dd6a1344b02d3c66b8bbdbb8ae120d6f2d4c5ab0
go-to-k <24818752+go-to-k@users.noreply.github.com>
2022-06-19 21:12:11 +0900

    first commit

---
+++ option/option.go
@@ -0,0 +1,7 @@
+package option
+
+type Option struct {
+	Profile   string `short:"p" long:"profile" description:"AWS profile name"`
+	StackName string `short:"s" long:"stackName" description:"Stack name" required:"true"`
+	Region    string `short:"r" long:"region" description:"AWS Region" default:"ap-northeast-1"`
+}
---
+++ shared/cloudformation.go
@@ -0,0 +1,86 @@
+package shared
+
+import (
+	"context"
+	"log"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
+)
+
+type CloudFormation struct {
+	client *cloudformation.Client
+	waiter *cloudformation.StackDeleteCompleteWaiter
+}
+
+func NewCloudFormation(cfg aws.Config) *CloudFormation {
+	client := cloudformation.NewFromConfig(cfg)
+	waiter := cloudformation.NewStackDeleteCompleteWaiter(client)
+	return &CloudFormation{
+		client,
+		waiter,
+	}
+}
+
+func (cfn *CloudFormation) DeleteStack(stackName *string) error {
+	input := &cloudformation.DeleteStackInput{
+		StackName: stackName,
+	}
+
+	_, err := cfn.client.DeleteStack(context.TODO(), input)
+	if err != nil {
+		log.Fatalf("failed delete the cloudformation stack, %v", err)
+		return err
+	}
+
+	if err := cfn.waitDeleteStack(stackName); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (cfn *CloudFormation) DescribeStacks(stackName *string) (*cloudformation.DescribeStacksOutput, bool, error) {
+	input := &cloudformation.DescribeStacksInput{
+		StackName: stackName,
+	}
+
+	output, err := cfn.client.DescribeStacks(context.TODO(), input)
+	if err != nil && strings.Contains(err.Error(), "does not exist") {
+		return output, false, nil
+	} else if err != nil {
+		log.Fatalf("failed describe the cloudformation stack, %v", err)
+		return output, true, err
+	}
+
+	return output, true, nil
+}
+
+func (cfn *CloudFormation) waitDeleteStack(stackName *string) error {
+	input := &cloudformation.DescribeStacksInput{
+		StackName: stackName,
+	}
+
+	err := cfn.waiter.Wait(context.TODO(), input, 3600000000000)
+	if err != nil && !strings.Contains(err.Error(), "waiter state transitioned to Failure") {
+		log.Fatalf("failed wait for stack deletion, %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func (cfn *CloudFormation) ListStackResources(stackName *string) (*cloudformation.ListStackResourcesOutput, error) {
+	input := &cloudformation.ListStackResourcesInput{
+		StackName: stackName,
+	}
+
+	output, err := cfn.client.ListStackResources(context.TODO(), input)
+	if err != nil {
+		log.Fatalf("failed list the cloudformation stack resources, %v", err)
+		return output, err
+	}
+
+	return output, nil
+}
