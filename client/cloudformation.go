3813b395e3671a2c011990a8bb21b852830a694f
goto <kenta.goto.1228@gmail.com>
2022-08-21 19:28:33 +0900

    refactor: change receivers name

--- client/cloudformation.go
+++ client/cloudformation.go
@@ -25,31 +25,31 @@ func NewCloudFormation(config aws.Config, stackName *string) *CloudFormation {
 	}
 }
 
-func (client *CloudFormation) DeleteStack(retainResources []string) error {
+func (cfn *CloudFormation) DeleteStack(retainResources []string) error {
 	input := &cloudformation.DeleteStackInput{
-		StackName:       client.stackName,
+		StackName:       cfn.stackName,
 		RetainResources: retainResources,
 	}
 
-	_, err := client.client.DeleteStack(context.TODO(), input)
+	_, err := cfn.client.DeleteStack(context.TODO(), input)
 	if err != nil {
 		log.Fatalf("failed delete the cloudformation stack, %v", err)
 		return err
 	}
 
-	if err := client.waitDeleteStack(); err != nil {
+	if err := cfn.waitDeleteStack(); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (client *CloudFormation) DescribeStacks() (*cloudformation.DescribeStacksOutput, bool, error) {
+func (cfn *CloudFormation) DescribeStacks() (*cloudformation.DescribeStacksOutput, bool, error) {
 	input := &cloudformation.DescribeStacksInput{
-		StackName: client.stackName,
+		StackName: cfn.stackName,
 	}
 
-	output, err := client.client.DescribeStacks(context.TODO(), input)
+	output, err := cfn.client.DescribeStacks(context.TODO(), input)
 	if err != nil && strings.Contains(err.Error(), "does not exist") {
 		return output, false, nil
 	} else if err != nil {
@@ -60,12 +60,12 @@ func (client *CloudFormation) DescribeStacks() (*cloudformation.DescribeStacksOu
 	return output, true, nil
 }
 
-func (client *CloudFormation) waitDeleteStack() error {
+func (cfn *CloudFormation) waitDeleteStack() error {
 	input := &cloudformation.DescribeStacksInput{
-		StackName: client.stackName,
+		StackName: cfn.stackName,
 	}
 
-	err := client.waiter.Wait(context.TODO(), input, 3600000000000)
+	err := cfn.waiter.Wait(context.TODO(), input, 3600000000000)
 	if err != nil && !strings.Contains(err.Error(), "waiter state transitioned to Failure") {
 		log.Fatalf("failed wait for stack deletion, %v", err)
 		return err
@@ -74,12 +74,12 @@ func (client *CloudFormation) waitDeleteStack() error {
 	return nil
 }
 
-func (client *CloudFormation) ListStackResources() (*cloudformation.ListStackResourcesOutput, error) {
+func (cfn *CloudFormation) ListStackResources() (*cloudformation.ListStackResourcesOutput, error) {
 	input := &cloudformation.ListStackResourcesInput{
-		StackName: client.stackName,
+		StackName: cfn.stackName,
 	}
 
-	output, err := client.client.ListStackResources(context.TODO(), input)
+	output, err := cfn.client.ListStackResources(context.TODO(), input)
 	if err != nil {
 		log.Fatalf("failed list the cloudformation stack resources, %v", err)
 		return output, err
