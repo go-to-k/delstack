c0b9cadde33690b09410918c01f6969841ac9923
goto <kenta.goto.1228@gmail.com>
2022-08-29 00:46:05 +0900

    chore: operations -> operation

--- operation/custom.go
+++ operation/custom.go
@@ -1,11 +1,11 @@
-package operations
+package operation
 
 import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/cloudformation/types"
 )
 
-func DeleteRoles(config aws.Config, resources []types.StackResourceSummary) error {
+func DeleteCustoms(config aws.Config, resources []types.StackResourceSummary) error {
 	// TODO: Concurrency Delete
 	return nil
 }
--- operation/ecr.go
+++ operation/ecr.go
@@ -1,4 +1,4 @@
-package operations
+package operation
 
 import (
 	"github.com/aws/aws-sdk-go-v2/aws"
