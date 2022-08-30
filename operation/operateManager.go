8c4370913e94cb3ffe6271c5ffc7a1e03324f8d2
go-to-k <24818752+go-to-k@users.noreply.github.com>
2022-08-30 16:05:53 +0900

    refactor: separete OperatorManager

---
+++ operation/operateManager.go
@@ -0,0 +1,49 @@
+package operation
+
+import (
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation/types"
+)
+
+type OperatorManager struct {
+	operatorCollection *OperatorCollection
+}
+
+func NewOperatorManager(config aws.Config, stackName string, stackResourceSummaries []types.StackResourceSummary) *OperatorManager {
+	return &OperatorManager{
+		operatorCollection: NewOperatorCollection(config, stackName, stackResourceSummaries),
+	}
+}
+
+func (operatorManager *OperatorManager) getResourcesLengthFromOperatorList() int {
+	var length int
+	for _, operator := range operatorManager.operatorCollection.OperatorList {
+		length += operator.GetResourcesLength()
+	}
+	return length
+}
+
+func (operatorManager *OperatorManager) CheckResourceCounts() error {
+	collectionLength := operatorManager.getResourcesLengthFromOperatorList()
+
+	if len(operatorManager.operatorCollection.LogicalResourceIds) != collectionLength {
+		return operatorManager.operatorCollection.GetNotSupportedServicesError()
+	}
+
+	return nil
+}
+
+func (operatorManager *OperatorManager) GetLogicalResourceIds() *[]string {
+	return operatorManager.operatorCollection.GetLogicalResourceIds()
+}
+
+func (operatorManager *OperatorManager) DeleteResourceCollection() error {
+	// TODO: Concurrency deletion of failed resources
+	for _, operator := range operatorManager.operatorCollection.OperatorList {
+		if err := operator.DeleteResources(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
