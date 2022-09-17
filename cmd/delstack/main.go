3e6d0c8e2cd499afe08314ecec859ef0f7855a86
go-to-k <24818752+go-to-k@users.noreply.github.com>
2022-09-18 03:16:26 +0900

    chore: version handling

--- cmd/delstack/main.go
+++ cmd/delstack/main.go
@@ -3,20 +3,32 @@ package main
 import (
 	"context"
 	"os"
+	"runtime/debug"
 
 	"github.com/go-to-k/delstack/app"
 	"github.com/go-to-k/delstack/logger"
 )
 
-var version = "version"
+var version = ""
 
 func main() {
 	logger.NewLogger()
 	ctx := context.TODO()
-	app := app.NewApp(version)
+	app := app.NewApp(getVersion())
 
 	if err := app.Run(ctx); err != nil {
 		logger.Logger.Error().Msg(err.Error())
 		os.Exit(1)
 	}
+}
+
+func getVersion() string {
+	if version != "" {
+		return version
+	}
+	i, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "unknown"
+	}
+	return i.Main.Version
 }
