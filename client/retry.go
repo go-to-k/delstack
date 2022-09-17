82aef19c509b96c7bffadc6f3c0579d0301a952b
go-to-k <24818752+go-to-k@users.noreply.github.com>
2022-09-18 05:12:46 +0900

    refactor: WaitForRetry

---
+++ client/retry.go
@@ -0,0 +1,22 @@
+package client
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/go-to-k/delstack/logger"
+	"github.com/go-to-k/delstack/option"
+)
+
+func WaitForRetry(retryCount int, sleepTimeSec int, targetResourceType *string, err error) error {
+	if retryCount > option.MaxRetryCount {
+		logger.Logger.Warn().Msg(err.Error() + "\nRetryCount(" + strconv.Itoa(option.MaxRetryCount) + ") over, but failed to delete. ")
+		return fmt.Errorf("RetryCountOverError: %v", targetResourceType)
+	}
+
+	logger.Logger.Warn().Msg(err.Error() + "\nRetrying...")
+	time.Sleep(time.Duration(sleepTimeSec) * time.Second)
+
+	return nil
+}
