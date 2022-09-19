741879189c2d36e1e192c333596843f1a01e38a2
go-to-k <24818752+go-to-k@users.noreply.github.com>
2022-09-19 21:14:09 +0900

    test: add operation collection tests

---
+++ operation/operator_collection_test.go
@@ -0,0 +1,180 @@
+package operation
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation/types"
+	"github.com/go-to-k/delstack/logger"
+)
+
+/*
+	Test Cases
+*/
+func TestSetOperatorCollection(t *testing.T) {
+	logger.NewLogger()
+	ctx := context.TODO()
+
+	type args struct {
+		ctx                    context.Context
+		stackName              *string
+		stackResourceSummaries []types.StackResourceSummary
+	}
+
+	type want struct {
+		logicalResourceIdsLength        int
+		unsupportedStackResourcesLength int
+	}
+
+	cases := []struct {
+		name string
+		args args
+		want want
+	}{
+		{
+			name: "resource counts check 1",
+			args: args{
+				ctx:       ctx,
+				stackName: aws.String("test"),
+				stackResourceSummaries: []types.StackResourceSummary{
+					{
+						LogicalResourceId:  aws.String("LogicalResourceId1"),
+						ResourceStatus:     "DELETE_FAILED",
+						ResourceType:       aws.String("AWS::CloudFormation::Stack"),
+						PhysicalResourceId: aws.String("PhysicalResourceId1"),
+					},
+					{
+						LogicalResourceId:  aws.String("LogicalResourceId2"),
+						ResourceStatus:     "DELETE_FAILED",
+						ResourceType:       aws.String("AWS::S3::Bucket"),
+						PhysicalResourceId: aws.String("PhysicalResourceId2"),
+					},
+					{
+						LogicalResourceId:  aws.String("LogicalResourceId3"),
+						ResourceStatus:     "DELETE_FAILED",
+						ResourceType:       aws.String("AWS::IAM::Role"),
+						PhysicalResourceId: aws.String("PhysicalResourceId3"),
+					},
+					{
+						LogicalResourceId:  aws.String("LogicalResourceId4"),
+						ResourceStatus:     "DELETE_FAILED",
+						ResourceType:       aws.String("AWS::ECR::Repository"),
+						PhysicalResourceId: aws.String("PhysicalResourceId4"),
+					},
+					{
+						LogicalResourceId:  aws.String("LogicalResourceId5"),
+						ResourceStatus:     "DELETE_FAILED",
+						ResourceType:       aws.String("AWS::Backup::BackupVault"),
+						PhysicalResourceId: aws.String("PhysicalResourceId5"),
+					},
+					{
+						LogicalResourceId:  aws.String("LogicalResourceId6"),
+						ResourceStatus:     "DELETE_FAILED",
+						ResourceType:       aws.String("Custom::CustomResource"),
+						PhysicalResourceId: aws.String("PhysicalResourceId6"),
+					},
+				},
+			},
+			want: want{
+				logicalResourceIdsLength:        6,
+				unsupportedStackResourcesLength: 0,
+			},
+		},
+		{
+			name: "resource counts check 2",
+			args: args{
+				ctx:       ctx,
+				stackName: aws.String("test"),
+				stackResourceSummaries: []types.StackResourceSummary{
+					{
+						LogicalResourceId:  aws.String("LogicalResourceId1"),
+						ResourceStatus:     "DELETE_FAILED",
+						ResourceType:       aws.String("AWS::CloudFormation::Stack"),
+						PhysicalResourceId: aws.String("PhysicalResourceId1"),
+					},
+					{
+						LogicalResourceId:  aws.String("LogicalResourceId2"),
+						ResourceStatus:     "DELETE_FAILED",
+						ResourceType:       aws.String("AWS::DynamoDB::Table"),
+						PhysicalResourceId: aws.String("PhysicalResourceId2"),
+					},
+				},
+			},
+			want: want{
+				logicalResourceIdsLength:        2,
+				unsupportedStackResourcesLength: 1,
+			},
+		},
+		{
+			name: "resource counts check 3",
+			args: args{
+				ctx:       ctx,
+				stackName: aws.String("test"),
+				stackResourceSummaries: []types.StackResourceSummary{
+					{
+						LogicalResourceId:  aws.String("LogicalResourceId1"),
+						ResourceStatus:     "DELETE_COMPLETE",
+						ResourceType:       aws.String("AWS::CloudFormation::Stack"),
+						PhysicalResourceId: aws.String("PhysicalResourceId1"),
+					},
+					{
+						LogicalResourceId:  aws.String("LogicalResourceId2"),
+						ResourceStatus:     "DELETE_FAILED",
+						ResourceType:       aws.String("AWS::DynamoDB::Table"),
+						PhysicalResourceId: aws.String("PhysicalResourceId2"),
+					},
+				},
+			},
+			want: want{
+				logicalResourceIdsLength:        1,
+				unsupportedStackResourcesLength: 1,
+			},
+		},
+		{
+			name: "resource counts check 4",
+			args: args{
+				ctx:       ctx,
+				stackName: aws.String("test"),
+				stackResourceSummaries: []types.StackResourceSummary{
+					{
+						LogicalResourceId:  aws.String("LogicalResourceId1"),
+						ResourceStatus:     "DELETE_FAILED",
+						ResourceType:       aws.String("AWS::CloudFormation::Stack"),
+						PhysicalResourceId: aws.String("PhysicalResourceId1"),
+					},
+					{
+						LogicalResourceId:  aws.String("LogicalResourceId2"),
+						ResourceStatus:     "DELETE_COMPLETE",
+						ResourceType:       aws.String("AWS::DynamoDB::Table"),
+						PhysicalResourceId: aws.String("PhysicalResourceId2"),
+					},
+				},
+			},
+			want: want{
+				logicalResourceIdsLength:        1,
+				unsupportedStackResourcesLength: 0,
+			},
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			config := aws.Config{}
+			operatorFactory := NewOperatorFactory(config)
+			operatorCollection := NewOperatorCollection(config, operatorFactory)
+
+			operatorCollection.SetOperatorCollection(tt.args.stackName, tt.args.stackResourceSummaries)
+
+			got := want{
+				logicalResourceIdsLength:        len(operatorCollection.logicalResourceIds),
+				unsupportedStackResourcesLength: len(operatorCollection.unsupportedStackResources),
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("got = %#v, want %#v", got, tt.want)
+			}
+		})
+	}
+}
