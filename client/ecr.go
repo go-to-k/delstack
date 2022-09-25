a1682b6bdf3be8029b525c8d5c4f7b41383e1774
go-to-k <24818752+go-to-k@users.noreply.github.com>
2022-09-25 21:37:42 +0900

    feat: check ecr exists

--- client/ecr.go
+++ client/ecr.go
@@ -8,12 +8,14 @@ import (
 
 type IEcr interface {
 	DeleteRepository(repositoryName *string) error
+	CheckEcrExists(repositoryName *string) (bool, error)
 }
 
 var _ IEcr = (*Ecr)(nil)
 
 type IEcrSDKClient interface {
 	DeleteRepository(ctx context.Context, params *ecr.DeleteRepositoryInput, optFns ...func(*ecr.Options)) (*ecr.DeleteRepositoryOutput, error)
+	DescribeRepositories(ctx context.Context, params *ecr.DescribeRepositoriesInput, optFns ...func(*ecr.Options)) (*ecr.DescribeRepositoriesOutput, error)
 }
 
 type Ecr struct {
@@ -35,4 +37,36 @@ func (ecrClient *Ecr) DeleteRepository(repositoryName *string) error {
 	_, err := ecrClient.client.DeleteRepository(context.TODO(), input)
 
 	return err
+}
+
+func (ecrClient *Ecr) CheckEcrExists(repositoryName *string) (bool, error) {
+	var nextToken *string
+
+	for {
+		input := &ecr.DescribeRepositoriesInput{
+			NextToken: nextToken,
+			RepositoryNames: []string{
+				*repositoryName,
+			},
+		}
+
+		output, err := ecrClient.client.DescribeRepositories(context.TODO(), input)
+		if err != nil {
+			return false, err
+		}
+
+		for _, repository := range output.Repositories {
+			if *repository.RepositoryName == *repositoryName {
+				return true, nil
+			}
+		}
+
+		nextToken = output.NextToken
+
+		if nextToken == nil {
+			break
+		}
+	}
+
+	return false, nil
 }
--- client/ecr_test.go
+++ client/ecr_test.go
@@ -3,15 +3,18 @@ package client
 import (
 	"context"
 	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ecr"
+	"github.com/aws/aws-sdk-go-v2/service/ecr/types"
 	"github.com/go-to-k/delstack/logger"
 )
 
 var _ IEcrSDKClient = (*MockEcrSDKClient)(nil)
 var _ IEcrSDKClient = (*ErrorMockEcrSDKClient)(nil)
+var _ IEcrSDKClient = (*NotExistsMockForDescribeRepositoriesEcrSDKClient)(nil)
 
 /*
 	Mocks for SDK Client
@@ -26,6 +29,17 @@ func (m *MockEcrSDKClient) DeleteRepository(ctx context.Context, params *ecr.Del
 	return nil, nil
 }
 
+func (m *MockEcrSDKClient) DescribeRepositories(ctx context.Context, params *ecr.DescribeRepositoriesInput, optFns ...func(*ecr.Options)) (*ecr.DescribeRepositoriesOutput, error) {
+	output := &ecr.DescribeRepositoriesOutput{
+		Repositories: []types.Repository{
+			{
+				RepositoryName: aws.String("test"),
+			},
+		},
+	}
+	return output, nil
+}
+
 type ErrorMockEcrSDKClient struct{}
 
 func NewErrorMockEcrSDKClient() *ErrorMockEcrSDKClient {
@@ -34,6 +48,31 @@ func NewErrorMockEcrSDKClient() *ErrorMockEcrSDKClient {
 
 func (m *ErrorMockEcrSDKClient) DeleteRepository(ctx context.Context, params *ecr.DeleteRepositoryInput, optFns ...func(*ecr.Options)) (*ecr.DeleteRepositoryOutput, error) {
 	return nil, fmt.Errorf("DeleteRepositoryError")
+}
+
+func (m *ErrorMockEcrSDKClient) DescribeRepositories(ctx context.Context, params *ecr.DescribeRepositoriesInput, optFns ...func(*ecr.Options)) (*ecr.DescribeRepositoriesOutput, error) {
+	return nil, fmt.Errorf("DescribeRepositoriesError")
+}
+
+type NotExistsMockForDescribeRepositoriesEcrSDKClient struct{}
+
+func NewNotExistsMockForDescribeRepositoriesEcrSDKClient() *NotExistsMockForDescribeRepositoriesEcrSDKClient {
+	return &NotExistsMockForDescribeRepositoriesEcrSDKClient{}
+}
+
+func (m *NotExistsMockForDescribeRepositoriesEcrSDKClient) DeleteRepository(ctx context.Context, params *ecr.DeleteRepositoryInput, optFns ...func(*ecr.Options)) (*ecr.DeleteRepositoryOutput, error) {
+	return nil, nil
+}
+
+func (m *NotExistsMockForDescribeRepositoriesEcrSDKClient) DescribeRepositories(ctx context.Context, params *ecr.DescribeRepositoriesInput, optFns ...func(*ecr.Options)) (*ecr.DescribeRepositoriesOutput, error) {
+	output := &ecr.DescribeRepositoriesOutput{
+		Repositories: []types.Repository{
+			{
+				RepositoryName: aws.String("test0"),
+			},
+		},
+	}
+	return output, nil
 }
 
 /*
@@ -91,6 +130,91 @@ func TestEcr_DeleteRepository(t *testing.T) {
 			}
 			if tt.wantErr && err.Error() != tt.want.Error() {
 				t.Errorf("err = %#v, want %#v", err, tt.want)
+			}
+		})
+	}
+}
+
+func TestEcr_CheckRepository(t *testing.T) {
+	logger.NewLogger(false)
+	ctx := context.TODO()
+	mock := NewMockEcrSDKClient()
+	errorMock := NewErrorMockEcrSDKClient()
+	notExitsMock := NewNotExistsMockForDescribeRepositoriesEcrSDKClient()
+
+	type args struct {
+		ctx            context.Context
+		repositoryName *string
+		client         IEcrSDKClient
+	}
+
+	type want struct {
+		exists bool
+		err    error
+	}
+
+	cases := []struct {
+		name    string
+		args    args
+		want    want
+		wantErr bool
+	}{
+		{
+			name: "check repository exists successfully",
+			args: args{
+				ctx:            ctx,
+				repositoryName: aws.String("test"),
+				client:         mock,
+			},
+			want: want{
+				exists: true,
+				err:    nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "check repository not exists successfully",
+			args: args{
+				ctx:            ctx,
+				repositoryName: aws.String("test"),
+				client:         notExitsMock,
+			},
+			want: want{
+				exists: false,
+				err:    nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "check repository exists failure",
+			args: args{
+				ctx:            ctx,
+				repositoryName: aws.String("test"),
+				client:         errorMock,
+			},
+			want: want{
+				exists: false,
+				err:    fmt.Errorf("DescribeRepositoriesError"),
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			ecrClient := NewEcr(tt.args.client)
+
+			output, err := ecrClient.CheckEcrExists(tt.args.repositoryName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("error = %#v, wantErr %#v", err.Error(), tt.wantErr)
+				return
+			}
+			if tt.wantErr && err.Error() != tt.want.err.Error() {
+				t.Errorf("err = %#v, want %#v", err.Error(), tt.want.err.Error())
+				return
+			}
+			if !reflect.DeepEqual(output, tt.want.exists) {
+				t.Errorf("output = %#v, want %#v", output, tt.want.exists)
 			}
 		})
 	}
