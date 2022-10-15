c4297efa1ee6c1ea96dc7f8a5e496e8b49ff53a3
go-to-k <24818752+go-to-k@users.noreply.github.com>
2022-10-16 01:51:53 +0900

    refactor: change directory layout

---
+++ internal/operation/bucket.go
@@ -0,0 +1,89 @@
+package operation
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation/types"
+	"github.com/go-to-k/delstack/internal/option"
+	"github.com/go-to-k/delstack/pkg/client"
+	"golang.org/x/sync/errgroup"
+	"golang.org/x/sync/semaphore"
+)
+
+var _ IOperator = (*BucketOperator)(nil)
+var sleepTimeSecForS3 = 10
+
+type BucketOperator struct {
+	client    client.IS3
+	resources []*types.StackResourceSummary
+}
+
+func NewBucketOperator(client client.IS3) *BucketOperator {
+	return &BucketOperator{
+		client:    client,
+		resources: []*types.StackResourceSummary{},
+	}
+}
+
+func (operator *BucketOperator) AddResource(resource *types.StackResourceSummary) {
+	operator.resources = append(operator.resources, resource)
+}
+
+func (operator *BucketOperator) GetResourcesLength() int {
+	return len(operator.resources)
+}
+
+func (operator *BucketOperator) DeleteResources() error {
+	var eg errgroup.Group
+	sem := semaphore.NewWeighted(int64(option.ConcurrencyNum))
+
+	for _, bucket := range operator.resources {
+		bucket := bucket
+		eg.Go(func() error {
+			sem.Acquire(context.Background(), 1)
+			defer sem.Release(1)
+
+			return operator.DeleteBucket(bucket.PhysicalResourceId)
+		})
+	}
+
+	return eg.Wait()
+}
+
+func (operator *BucketOperator) DeleteBucket(bucketName *string) error {
+	exists, err := operator.client.CheckBucketExists(bucketName)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return nil
+	}
+
+	versions, err := operator.client.ListObjectVersions(bucketName)
+	if err != nil {
+		return err
+	}
+
+	if len(versions) > 0 {
+		errors, err := operator.client.DeleteObjects(bucketName, versions, sleepTimeSecForS3)
+		if err != nil {
+			return err
+		}
+		if len(errors) > 0 {
+			errorStr := ""
+			for _, error := range errors {
+				errorStr += fmt.Sprintf("\nCode: %v\n", *error.Code)
+				errorStr += fmt.Sprintf("Key: %v\n", *error.Key)
+				errorStr += fmt.Sprintf("VersionId: %v\n", *error.VersionId)
+				errorStr += fmt.Sprintf("Message: %v\n", *error.Message)
+			}
+			return fmt.Errorf("DeleteObjectsError: followings %v", errorStr)
+		}
+	}
+	if err := operator.client.DeleteBucket(bucketName); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ pkg/client/cloudformation.go
@@ -0,0 +1,110 @@
+package client
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation/types"
+	"github.com/go-to-k/delstack/internal/option"
+)
+
+type ICloudFormation interface {
+	DeleteStack(stackName *string, retainResources []string) error
+	DescribeStacks(stackName *string) (*cloudformation.DescribeStacksOutput, bool, error)
+	ListStackResources(stackName *string) ([]types.StackResourceSummary, error)
+}
+
+var _ ICloudFormation = (*CloudFormation)(nil)
+
+type ICloudFormationSDKClient interface {
+	DeleteStack(ctx context.Context, params *cloudformation.DeleteStackInput, optFns ...func(*cloudformation.Options)) (*cloudformation.DeleteStackOutput, error)
+	DescribeStacks(ctx context.Context, params *cloudformation.DescribeStacksInput, optFns ...func(*cloudformation.Options)) (*cloudformation.DescribeStacksOutput, error)
+	ListStackResources(ctx context.Context, params *cloudformation.ListStackResourcesInput, optFns ...func(*cloudformation.Options)) (*cloudformation.ListStackResourcesOutput, error)
+}
+
+type ICloudFormationSDKWaiter interface {
+	Wait(ctx context.Context, params *cloudformation.DescribeStacksInput, maxWaitDur time.Duration, optFns ...func(*cloudformation.StackDeleteCompleteWaiterOptions)) error
+}
+
+type CloudFormation struct {
+	client ICloudFormationSDKClient
+	waiter ICloudFormationSDKWaiter
+}
+
+func NewCloudFormation(client ICloudFormationSDKClient, waiter ICloudFormationSDKWaiter) *CloudFormation {
+	return &CloudFormation{
+		client,
+		waiter,
+	}
+}
+
+func (cfnClient *CloudFormation) DeleteStack(stackName *string, retainResources []string) error {
+	input := &cloudformation.DeleteStackInput{
+		StackName:       stackName,
+		RetainResources: retainResources,
+	}
+
+	if _, err := cfnClient.client.DeleteStack(context.TODO(), input); err != nil {
+		return err
+	}
+
+	if err := cfnClient.waitDeleteStack(stackName); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (cfnClient *CloudFormation) DescribeStacks(stackName *string) (*cloudformation.DescribeStacksOutput, bool, error) {
+	input := &cloudformation.DescribeStacksInput{
+		StackName: stackName,
+	}
+
+	output, err := cfnClient.client.DescribeStacks(context.TODO(), input)
+	if err != nil && strings.Contains(err.Error(), "does not exist") {
+		return output, false, nil
+	}
+
+	return output, true, err
+}
+
+func (cfnClient *CloudFormation) waitDeleteStack(stackName *string) error {
+	input := &cloudformation.DescribeStacksInput{
+		StackName: stackName,
+	}
+
+	err := cfnClient.waiter.Wait(context.TODO(), input, option.CloudFormationWaitNanoSecTime)
+	if err != nil && !strings.Contains(err.Error(), "waiter state transitioned to Failure") {
+		return err
+	}
+
+	return nil
+}
+
+func (cfnClient *CloudFormation) ListStackResources(stackName *string) ([]types.StackResourceSummary, error) {
+	var nextToken *string
+	stackResourceSummaries := []types.StackResourceSummary{}
+
+	for {
+		input := &cloudformation.ListStackResourcesInput{
+			StackName: stackName,
+			NextToken: nextToken,
+		}
+
+		output, err := cfnClient.client.ListStackResources(context.TODO(), input)
+		if err != nil {
+			return stackResourceSummaries, err
+		}
+
+		stackResourceSummaries = append(stackResourceSummaries, output.StackResourceSummaries...)
+		nextToken = output.NextToken
+
+		if nextToken == nil {
+			break
+		}
+	}
+
+	return stackResourceSummaries, nil
+}
