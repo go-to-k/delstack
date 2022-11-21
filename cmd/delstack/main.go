4212e5aba6cdd1fc97a3106cd96862c40988b235
goto <kenta.goto.1228@gmail.com>
2022-11-21 19:34:20 +0900

    refactor: some funcs into option pkg

--- cmd/delstack/main.go
+++ cmd/delstack/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"os"
-	"runtime/debug"
 
 	"github.com/go-to-k/delstack/internal/app"
 	"github.com/go-to-k/delstack/internal/option"
@@ -11,34 +10,12 @@ import (
 )
 
 func main() {
-	logger.NewLogger(isDebug())
+	logger.NewLogger(option.IsDebug())
 	ctx := context.TODO()
-	app := app.NewApp(getVersion())
+	app := app.NewApp(option.GetVersion())
 
 	if err := app.Run(ctx); err != nil {
 		logger.Logger.Error().Msg(err.Error())
 		os.Exit(1)
 	}
-}
-
-func isDebug() bool {
-	if option.Version == "" || option.Revision != "" {
-		return true
-	}
-	return false
-}
-
-func getVersion() string {
-	if option.Version != "" && option.Revision != "" {
-		return option.Version + "-" + option.Revision
-	}
-	if option.Version != "" {
-		return option.Version
-	}
-
-	i, ok := debug.ReadBuildInfo()
-	if !ok {
-		return "unknown"
-	}
-	return i.Main.Version
 }
