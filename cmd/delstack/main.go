8df032228ce89e0b58bc2dd5d468c50b039a39cb
goto <kenta.goto.1228@gmail.com>
2022-11-21 20:15:51 +0900

    refactor: change package name(option -> version)

--- cmd/delstack/main.go
+++ cmd/delstack/main.go
@@ -5,14 +5,14 @@ import (
 	"os"
 
 	"github.com/go-to-k/delstack/internal/app"
-	"github.com/go-to-k/delstack/internal/option"
+	"github.com/go-to-k/delstack/internal/version"
 	"github.com/go-to-k/delstack/pkg/logger"
 )
 
 func main() {
-	logger.NewLogger(option.IsDebug())
+	logger.NewLogger(version.IsDebug())
 	ctx := context.TODO()
-	app := app.NewApp(option.GetVersion())
+	app := app.NewApp(version.GetVersion())
 
 	if err := app.Run(ctx); err != nil {
 		logger.Logger.Error().Msg(err.Error())
--- internal/version/version.go
+++ internal/version/version.go
@@ -1,4 +1,4 @@
-package option
+package version
 
 import (
 	"runtime/debug"
