e6a9cb80b91cd024ec3b9639f8f8bcd6886ca545
goto <kenta.goto.1228@gmail.com>
2022-12-31 03:26:11 +0900

    test: add TestS3_deleteObjectsWithRetry

--- pkg/client/s3_test.go
+++ pkg/client/s3_test.go
@@ -551,6 +551,190 @@ func TestS3_DeleteObjects(t *testing.T) {
 	}
 }
 
+func TestS3_deleteObjectsWithRetry(t *testing.T) {
+	ctx := context.Background()
+	mock := NewMockS3SDKClient()
+	errorMock := NewErrorMockS3SDKClient()
+	apiErrorMock := NewApiErrorMockS3SDKClient()
+	outputErrorMock := NewOutputErrorForDeleteObjectsMockS3SDKClient()
+
+	objectsOverLimit := []types.ObjectIdentifier{}
+	s3DeleteObjectsSizeOverLimit := s3DeleteObjectsSizeLimit*int(runtime.NumCPU())*2 + 1 // loop over cpu core size for channel waiting when next loop
+	for i := 0; i < s3DeleteObjectsSizeOverLimit; i++ {
+		objectsOverLimit = append(objectsOverLimit, types.ObjectIdentifier{
+			Key:       aws.String("Key"),
+			VersionId: aws.String("VersionId"),
+		})
+	}
+
+	type args struct {
+		ctx        context.Context
+		input      *s3.DeleteObjectsInput
+		bucketName *string
+		client     IS3SDKClient
+	}
+
+	type want struct {
+		output *s3.DeleteObjectsOutput
+		err    error
+	}
+
+	cases := []struct {
+		name    string
+		args    args
+		want    want
+		wantErr bool
+	}{
+		{
+			name: "delete objects successfully",
+			args: args{
+				ctx: ctx,
+				input: &s3.DeleteObjectsInput{
+					Bucket: aws.String("test"),
+					Delete: &types.Delete{
+						Objects: []types.ObjectIdentifier{{
+							Key:       aws.String("Key"),
+							VersionId: aws.String("VersionId")},
+						},
+						Quiet: *aws.Bool(true),
+					},
+				},
+				bucketName: aws.String("test"),
+				client:     mock,
+			},
+			want: want{
+				output: &s3.DeleteObjectsOutput{
+					Deleted: []types.DeletedObject{},
+					Errors:  []types.Error{},
+				},
+				err: nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "delete objects over limit successfully",
+			args: args{
+				ctx: ctx,
+				input: &s3.DeleteObjectsInput{
+					Bucket: aws.String("test"),
+					Delete: &types.Delete{
+						Objects: objectsOverLimit,
+						Quiet:   *aws.Bool(true),
+					},
+				},
+				bucketName: aws.String("test"),
+				client:     mock,
+			},
+			want: want{
+				output: &s3.DeleteObjectsOutput{
+					Deleted: []types.DeletedObject{},
+					Errors:  []types.Error{},
+				},
+				err: nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "delete objects failure",
+			args: args{
+				ctx: ctx,
+				input: &s3.DeleteObjectsInput{
+					Bucket: aws.String("test"),
+					Delete: &types.Delete{
+						Objects: []types.ObjectIdentifier{{
+							Key:       aws.String("Key"),
+							VersionId: aws.String("VersionId")},
+						},
+						Quiet: *aws.Bool(true),
+					},
+				},
+				bucketName: aws.String("test"),
+				client:     errorMock,
+			},
+			want: want{
+				output: nil,
+				err:    fmt.Errorf("DeleteObjectsError"),
+			},
+			wantErr: true,
+		},
+		{
+			name: "delete objects failure for api error",
+			args: args{
+				ctx: ctx,
+				input: &s3.DeleteObjectsInput{
+					Bucket: aws.String("test"),
+					Delete: &types.Delete{
+						Objects: []types.ObjectIdentifier{{
+							Key:       aws.String("Key"),
+							VersionId: aws.String("VersionId")},
+						},
+						Quiet: *aws.Bool(true),
+					},
+				},
+				bucketName: aws.String("test"),
+				client:     apiErrorMock,
+			},
+			want: want{
+				output: nil,
+				err:    fmt.Errorf("RetryCountOverError: test, api error SlowDown\nRetryCount(" + strconv.Itoa(maxRetryCount) + ") over, but failed to delete. "),
+			},
+			wantErr: true,
+		},
+		{
+			name: "delete objects failure for output errors",
+			args: args{
+				ctx: ctx,
+				input: &s3.DeleteObjectsInput{
+					Bucket: aws.String("test"),
+					Delete: &types.Delete{
+						Objects: []types.ObjectIdentifier{{
+							Key:       aws.String("Key"),
+							VersionId: aws.String("VersionId")},
+						},
+						Quiet: *aws.Bool(true),
+					},
+				},
+				bucketName: aws.String("test"),
+				client:     outputErrorMock,
+			},
+			want: want{
+				output: &s3.DeleteObjectsOutput{
+					Deleted: []types.DeletedObject{},
+					Errors: []types.Error{
+						{
+							Key:       aws.String("Key"),
+							Code:      aws.String("Code"),
+							Message:   aws.String("Message"),
+							VersionId: aws.String("VersionId"),
+						},
+					},
+				},
+				err: nil,
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			s3Client := NewS3(tt.args.client)
+
+			output, err := s3Client.deleteObjectsWithRetry(tt.args.ctx, tt.args.input, tt.args.bucketName, sleepTimeSecForS3)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("error = %#v, wantErr %#v", err.Error(), tt.wantErr)
+				return
+			}
+			if tt.wantErr && err.Error() != tt.want.err.Error() {
+				t.Errorf("err = %#v, want %#v", err.Error(), tt.want.err.Error())
+				return
+			}
+			if !reflect.DeepEqual(output, tt.want.output) {
+				t.Errorf("output = %#v, want %#v", output, tt.want.output)
+			}
+		})
+	}
+}
+
 func TestS3_ListObjectVersions(t *testing.T) {
 	ctx := context.Background()
 	mock := NewMockS3SDKClient()
