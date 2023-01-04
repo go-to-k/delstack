eb45086134f12ef1214325a1989a9c0b4861e97e
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-01-05 00:22:50 +0900

    refactor: handling ctx.Done in some clients (#57)
    * refactor: handling ctx.Done in some clients
    
    * refactor: add retry funcs in iam client
    
    * test: add retry funcs tests in iam client

--- pkg/client/iam_test.go
+++ pkg/client/iam_test.go
@@ -181,7 +181,7 @@ func TestIam_DeleteRole(t *testing.T) {
 			wantErr: true,
 		},
 		{
-			name: "delete role failure  for api error",
+			name: "delete role failure for api error",
 			args: args{
 				ctx:      ctx,
 				roleName: aws.String("test"),
@@ -197,6 +197,82 @@ func TestIam_DeleteRole(t *testing.T) {
 			iamClient := NewIam(tt.args.client)
 
 			err := iamClient.DeleteRole(tt.args.ctx, tt.args.roleName, sleepTimeSecForIam)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("error = %#v, wantErr %#v", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr && err.Error() != tt.want.Error() {
+				t.Errorf("err = %#v, want %#v", err, tt.want)
+			}
+		})
+	}
+}
+
+func TestIam_deleteRoleWithRetry(t *testing.T) {
+	ctx := context.Background()
+	mock := NewMockIamSDKClient()
+	errorMock := NewErrorMockIamSDKClient()
+	apiErrorMock := NewApiErrorMockIamSDKClient()
+
+	type args struct {
+		ctx      context.Context
+		input    *iam.DeleteRoleInput
+		roleName *string
+		client   IIamSDKClient
+	}
+
+	cases := []struct {
+		name    string
+		args    args
+		want    error
+		wantErr bool
+	}{
+		{
+			name: "delete role successfully",
+			args: args{
+				ctx: ctx,
+				input: &iam.DeleteRoleInput{
+					RoleName: aws.String("test"),
+				},
+				roleName: aws.String("test"),
+				client:   mock,
+			},
+			want:    nil,
+			wantErr: false,
+		},
+		{
+			name: "delete role failure",
+			args: args{
+				ctx: ctx,
+				input: &iam.DeleteRoleInput{
+					RoleName: aws.String("test"),
+				},
+				roleName: aws.String("test"),
+				client:   errorMock,
+			},
+			want:    fmt.Errorf("DeleteRoleError"),
+			wantErr: true,
+		},
+		{
+			name: "delete role failure for api error",
+			args: args{
+				ctx: ctx,
+				input: &iam.DeleteRoleInput{
+					RoleName: aws.String("test"),
+				},
+				roleName: aws.String("test"),
+				client:   apiErrorMock,
+			},
+			want:    fmt.Errorf("RetryCountOverError: test, api error Throttling: Rate exceeded\nRetryCount(" + strconv.Itoa(maxRetryCount) + ") over, but failed to delete. "),
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			iamClient := NewIam(tt.args.client)
+
+			_, err := iamClient.deleteRoleWithRetry(tt.args.ctx, tt.args.input, tt.args.roleName, sleepTimeSecForIam)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("error = %#v, wantErr %#v", err, tt.wantErr)
 				return
@@ -454,6 +530,85 @@ func TestIam_DetachRolePolicy(t *testing.T) {
 			iamClient := NewIam(tt.args.client)
 
 			err := iamClient.DetachRolePolicy(tt.args.ctx, tt.args.roleName, tt.args.PolicyArn, sleepTimeSecForIam)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("error = %#v, wantErr %#v", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr && err.Error() != tt.want.Error() {
+				t.Errorf("err = %#v, want %#v", err, tt.want)
+			}
+		})
+	}
+}
+
+func TestIam_detachRolePolicyWithRetry(t *testing.T) {
+	ctx := context.Background()
+	mock := NewMockIamSDKClient()
+	errorMock := NewErrorMockIamSDKClient()
+	apiErrorMock := NewApiErrorMockIamSDKClient()
+
+	type args struct {
+		ctx      context.Context
+		input    *iam.DetachRolePolicyInput
+		roleName *string
+		client   IIamSDKClient
+	}
+
+	cases := []struct {
+		name    string
+		args    args
+		want    error
+		wantErr bool
+	}{
+		{
+			name: "detach role policy successfully",
+			args: args{
+				ctx: ctx,
+				input: &iam.DetachRolePolicyInput{
+					PolicyArn: aws.String("test"),
+					RoleName:  aws.String("test"),
+				},
+				roleName: aws.String("test"),
+				client:   mock,
+			},
+			want:    nil,
+			wantErr: false,
+		},
+		{
+			name: "detach role policy failure",
+			args: args{
+				ctx: ctx,
+				input: &iam.DetachRolePolicyInput{
+					PolicyArn: aws.String("test"),
+					RoleName:  aws.String("test"),
+				},
+				roleName: aws.String("test"),
+				client:   errorMock,
+			},
+			want:    fmt.Errorf("DetachRolePolicyError"),
+			wantErr: true,
+		},
+		{
+			name: "detach role policy failure for api error",
+			args: args{
+				ctx: ctx,
+				input: &iam.DetachRolePolicyInput{
+					PolicyArn: aws.String("test"),
+					RoleName:  aws.String("test"),
+				},
+				roleName: aws.String("test"),
+				client:   apiErrorMock,
+			},
+			want:    fmt.Errorf("RetryCountOverError: test, api error Throttling: Rate exceeded\nRetryCount(" + strconv.Itoa(maxRetryCount) + ") over, but failed to delete. "),
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			iamClient := NewIam(tt.args.client)
+
+			_, err := iamClient.detachRolePolicyWithRetry(tt.args.ctx, tt.args.input, tt.args.roleName, sleepTimeSecForIam)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("error = %#v, wantErr %#v", err, tt.wantErr)
 				return
