0240c8e8577c4045eb909150e699ad96cf193952
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-01-12 12:17:41 +0900

    test: mock files separation (#69)

--- internal/operation/ecr_test.go
+++ internal/operation/ecr_test.go
@@ -17,67 +17,9 @@ var _ client.IEcr = (*CheckEcrExistsErrorMockEcr)(nil)
 var _ client.IEcr = (*CheckEcrNotExistsMockEcr)(nil)
 
 /*
-	Mocks for client
-*/
-type MockEcr struct{}
-
-func NewMockEcr() *MockEcr {
-	return &MockEcr{}
-}
-
-func (m *MockEcr) DeleteRepository(ctx context.Context, repositoryName *string) error {
-	return nil
-}
-
-func (m *MockEcr) CheckEcrExists(ctx context.Context, repositoryName *string) (bool, error) {
-	return true, nil
-}
-
-type DeleteRepositoryErrorMockEcr struct{}
-
-func NewDeleteRepositoryErrorMockEcr() *DeleteRepositoryErrorMockEcr {
-	return &DeleteRepositoryErrorMockEcr{}
-}
-
-func (m *DeleteRepositoryErrorMockEcr) DeleteRepository(ctx context.Context, repositoryName *string) error {
-	return fmt.Errorf("DeleteRepositoryError")
-}
-
-func (m *DeleteRepositoryErrorMockEcr) CheckEcrExists(ctx context.Context, repositoryName *string) (bool, error) {
-	return true, nil
-}
-
-type CheckEcrExistsErrorMockEcr struct{}
-
-func NewCheckEcrExistsErrorMockEcr() *CheckEcrExistsErrorMockEcr {
-	return &CheckEcrExistsErrorMockEcr{}
-}
-
-func (m *CheckEcrExistsErrorMockEcr) DeleteRepository(ctx context.Context, repositoryName *string) error {
-	return nil
-}
-
-func (m *CheckEcrExistsErrorMockEcr) CheckEcrExists(ctx context.Context, repositoryName *string) (bool, error) {
-	return false, fmt.Errorf("DescribeRepositoriesError")
-}
-
-type CheckEcrNotExistsMockEcr struct{}
-
-func NewCheckEcrNotExistsMockEcr() *CheckEcrNotExistsMockEcr {
-	return &CheckEcrNotExistsMockEcr{}
-}
-
-func (m *CheckEcrNotExistsMockEcr) DeleteRepository(ctx context.Context, repositoryName *string) error {
-	return nil
-}
-
-func (m *CheckEcrNotExistsMockEcr) CheckEcrExists(ctx context.Context, repositoryName *string) (bool, error) {
-	return false, nil
-}
-
-/*
 	Test Cases
 */
+
 func TestEcrOperator_DeleteRepository(t *testing.T) {
 	io.NewLogger(false)
 	mock := NewMockEcr()
