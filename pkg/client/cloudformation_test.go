9049dd504b1b932aded88427f4c3c7a4d2351498
goto <kenta.goto.1228@gmail.com>
2023-01-19 16:46:17 +0900

    test: change TestCloudFormation_DeleteStack for unuse mocks

--- pkg/client/cloudformation_test.go
+++ pkg/client/cloudformation_test.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -39,7 +40,7 @@ func TestCloudFormation_DeleteStack(t *testing.T) {
 				withAPIOptionsFunc: func(stack *middleware.Stack) error {
 					return stack.Finalize.Add(
 						middleware.FinalizeMiddlewareFunc(
-							"DeleteStackOrDescribeStacksMock",
+							"DeleteStackOrDescribeStacksForWaiterMock",
 							func(ctx context.Context, input middleware.FinalizeInput, handler middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
 								operationName := awsMiddleware.GetOperationName(ctx)
 								if operationName == "DeleteStack" {
@@ -69,98 +70,132 @@ func TestCloudFormation_DeleteStack(t *testing.T) {
 			want:    nil,
 			wantErr: false,
 		},
-		// {
-		// 	name: "delete stack successfully including non retainResources",
-		// 	args: args{
-		// 		ctx:             context.Background(),
-		// 		stackName:       aws.String("test"),
-		// 		retainResources: []string{},
-		// 		withAPIOptionsFunc: func(stack *middleware.Stack) error {
-		// 			return stack.Finalize.Add(
-		// 				middleware.FinalizeMiddlewareFunc(
-		// 					"DeleteStackIncludingNonRetainResourcesMock",
-		// 					func(context.Context, middleware.FinalizeInput, middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
-		// 						return middleware.FinalizeOutput{
-		// 							Result: &cloudformation.DeleteStackOutput{},
-		// 						}, middleware.Metadata{}, nil
-		// 					},
-		// 				),
-		// 				middleware.Before,
-		// 			)
-		// 		},
-		// 	},
-		// 	want:    nil,
-		// 	wantErr: false,
-		// },
-		// {
-		// 	name: "delete stack successfully for transitioned to Failure",
-		// 	args: args{
-		// 		ctx:             context.Background(),
-		// 		stackName:       aws.String("test"),
-		// 		retainResources: []string{"test1", "test2"},
-		// 		withAPIOptionsFunc: func(stack *middleware.Stack) error {
-		// 			return stack.Finalize.Add(
-		// 				middleware.FinalizeMiddlewareFunc(
-		// 					"WaiterStateTransitionedToFailureMock",
-		// 					func(context.Context, middleware.FinalizeInput, middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
-		// 						return middleware.FinalizeOutput{
-		// 							Result: &cloudformation.DeleteStackOutput{},
-		// 						}, middleware.Metadata{}, fmt.Errorf("waiter state transitioned to Failure")
-		// 					},
-		// 				),
-		// 				middleware.Before,
-		// 			)
-		// 		},
-		// 	},
-		// 	want:    nil,
-		// 	wantErr: false,
-		// },
-		// {
-		// 	name: "delete stack failure",
-		// 	args: args{
-		// 		ctx:             context.Background(),
-		// 		stackName:       aws.String("test"),
-		// 		retainResources: []string{"test1", "test2"},
-		// 		withAPIOptionsFunc: func(stack *middleware.Stack) error {
-		// 			return stack.Finalize.Add(
-		// 				middleware.FinalizeMiddlewareFunc(
-		// 					"DeleteStackErrorMock",
-		// 					func(context.Context, middleware.FinalizeInput, middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
-		// 						return middleware.FinalizeOutput{
-		// 							Result: &cloudformation.DeleteStackOutput{},
-		// 						}, middleware.Metadata{}, fmt.Errorf("DeleteStackError")
-		// 					},
-		// 				),
-		// 				middleware.Before,
-		// 			)
-		// 		},
-		// 	},
-		// 	want:    fmt.Errorf("operation error CloudFormation: DeleteStack, DeleteStackError"),
-		// 	wantErr: true,
-		// },
-		// {
-		// 	name: "delete stack failure for wait errors",
-		// 	args: args{
-		// 		ctx:             context.Background(),
-		// 		stackName:       aws.String("test"),
-		// 		retainResources: []string{"test1", "test2"},
-		// 		withAPIOptionsFunc: func(stack *middleware.Stack) error {
-		// 			return stack.Finalize.Add(
-		// 				middleware.FinalizeMiddlewareFunc(
-		// 					"WaitErrorMock",
-		// 					func(context.Context, middleware.FinalizeInput, middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
-		// 						return middleware.FinalizeOutput{
-		// 							Result: &cloudformation.DeleteStackOutput{},
-		// 						}, middleware.Metadata{}, fmt.Errorf("WaitError")
-		// 					},
-		// 				),
-		// 				middleware.Before,
-		// 			)
-		// 		},
-		// 	},
-		// 	want:    fmt.Errorf("operation error CloudFormation: Wait, WaitError"),
-		// 	wantErr: true,
-		// },
+		{
+			name: "delete stack successfully including non retainResources",
+			args: args{
+				ctx:             context.Background(),
+				stackName:       aws.String("test"),
+				retainResources: []string{},
+				withAPIOptionsFunc: func(stack *middleware.Stack) error {
+					return stack.Finalize.Add(
+						middleware.FinalizeMiddlewareFunc(
+							"DeleteStackOrDescribeStacksForWaiterIncludingNonRetainResourcesMock",
+							func(ctx context.Context, input middleware.FinalizeInput, handler middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
+								operationName := awsMiddleware.GetOperationName(ctx)
+								if operationName == "DeleteStack" {
+									return middleware.FinalizeOutput{
+										Result: &cloudformation.DeleteStackOutput{},
+									}, middleware.Metadata{}, nil
+								}
+								if operationName == "DescribeStacks" {
+									return middleware.FinalizeOutput{
+										Result: &cloudformation.DescribeStacksOutput{
+											Stacks: []types.Stack{
+												{
+													StackName:   aws.String("StackName"),
+													StackStatus: "DELETE_COMPLETE",
+												},
+											},
+										},
+									}, middleware.Metadata{}, nil
+								}
+								return middleware.FinalizeOutput{}, middleware.Metadata{}, nil
+							},
+						),
+						middleware.Before,
+					)
+				},
+			},
+			want:    nil,
+			wantErr: false,
+		},
+		{
+			name: "delete stack successfully for transitioned to Failure",
+			args: args{
+				ctx:             context.Background(),
+				stackName:       aws.String("test"),
+				retainResources: []string{"test1", "test2"},
+				withAPIOptionsFunc: func(stack *middleware.Stack) error {
+					return stack.Finalize.Add(
+						middleware.FinalizeMiddlewareFunc(
+							"DeleteStackOrDescribeStacksForWaiterStateTransitionedToFailureMock",
+							func(ctx context.Context, input middleware.FinalizeInput, handler middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
+								operationName := awsMiddleware.GetOperationName(ctx)
+								if operationName == "DeleteStack" {
+									return middleware.FinalizeOutput{
+										Result: &cloudformation.DeleteStackOutput{},
+									}, middleware.Metadata{}, nil
+								}
+								if operationName == "DescribeStacks" {
+									return middleware.FinalizeOutput{
+										Result: &cloudformation.DescribeStacksOutput{},
+									}, middleware.Metadata{}, fmt.Errorf("waiter state transitioned to Failure")
+								}
+								return middleware.FinalizeOutput{}, middleware.Metadata{}, nil
+							},
+						),
+						middleware.Before,
+					)
+				},
+			},
+			want:    nil,
+			wantErr: false,
+		},
+		{
+			name: "delete stack failure",
+			args: args{
+				ctx:             context.Background(),
+				stackName:       aws.String("test"),
+				retainResources: []string{"test1", "test2"},
+				withAPIOptionsFunc: func(stack *middleware.Stack) error {
+					return stack.Finalize.Add(
+						middleware.FinalizeMiddlewareFunc(
+							"DeleteStackErrorMock",
+							func(context.Context, middleware.FinalizeInput, middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
+								return middleware.FinalizeOutput{
+									Result: &cloudformation.DeleteStackOutput{},
+								}, middleware.Metadata{}, fmt.Errorf("DeleteStackError")
+							},
+						),
+						middleware.Before,
+					)
+				},
+			},
+			want:    fmt.Errorf("operation error CloudFormation: DeleteStack, DeleteStackError"),
+			wantErr: true,
+		},
+		{
+			name: "delete stack failure for wait errors",
+			args: args{
+				ctx:             context.Background(),
+				stackName:       aws.String("test"),
+				retainResources: []string{"test1", "test2"},
+				withAPIOptionsFunc: func(stack *middleware.Stack) error {
+					return stack.Finalize.Add(
+						middleware.FinalizeMiddlewareFunc(
+							"DeleteStackOrDescribeStacksForWaiterErrorMock",
+							func(ctx context.Context, input middleware.FinalizeInput, handler middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
+								operationName := awsMiddleware.GetOperationName(ctx)
+								if operationName == "DeleteStack" {
+									return middleware.FinalizeOutput{
+										Result: &cloudformation.DeleteStackOutput{},
+									}, middleware.Metadata{}, nil
+								}
+								if operationName == "DescribeStacks" {
+									return middleware.FinalizeOutput{
+										Result: &cloudformation.DescribeStacksOutput{},
+									}, middleware.Metadata{}, fmt.Errorf("WaitError")
+								}
+								return middleware.FinalizeOutput{}, middleware.Metadata{}, nil
+							},
+						),
+						middleware.Before,
+					)
+				},
+			},
+			want:    fmt.Errorf("expected err to be of type smithy.APIError, got %w", fmt.Errorf("operation error CloudFormation: DescribeStacks, WaitError")),
+			wantErr: true,
+		},
 	}
 
 	for _, tt := range cases {
