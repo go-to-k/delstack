fcb94e0e16f8aa9fe41bc72f38ce7fdff6ac3b11
goto <kenta.goto.1228@gmail.com>
2023-01-20 00:34:25 +0900

    test: change mock name for some error mocks

--- pkg/client/iam_test.go
+++ pkg/client/iam_test.go
@@ -85,7 +85,7 @@ func TestIam_DeleteRole(t *testing.T) {
 				withAPIOptionsFunc: func(stack *middleware.Stack) error {
 					return stack.Finalize.Add(
 						middleware.FinalizeMiddlewareFunc(
-							"DeleteRoleForApiErrorMock",
+							"DeleteRoleApiErrorMock",
 							func(context.Context, middleware.FinalizeInput, middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
 								return middleware.FinalizeOutput{
 									Result: &iam.DeleteRoleOutput{},
@@ -202,7 +202,7 @@ func TestIam_deleteRoleWithRetry(t *testing.T) {
 				withAPIOptionsFunc: func(stack *middleware.Stack) error {
 					return stack.Finalize.Add(
 						middleware.FinalizeMiddlewareFunc(
-							"DeleteRoleForApiErrorMock",
+							"DeleteRoleApiErrorMock",
 							func(context.Context, middleware.FinalizeInput, middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
 								return middleware.FinalizeOutput{
 									Result: &iam.DeleteRoleOutput{},
@@ -483,7 +483,7 @@ func TestIam_DetachRolePolicies(t *testing.T) {
 				withAPIOptionsFunc: func(stack *middleware.Stack) error {
 					return stack.Finalize.Add(
 						middleware.FinalizeMiddlewareFunc(
-							"DetachRolePolicyForApiErrorMock",
+							"DetachRolePolicyApiErrorMock",
 							func(context.Context, middleware.FinalizeInput, middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
 								return middleware.FinalizeOutput{
 									Result: &iam.DetachRolePolicyOutput{},
@@ -594,7 +594,7 @@ func TestIam_DetachRolePolicy(t *testing.T) {
 				withAPIOptionsFunc: func(stack *middleware.Stack) error {
 					return stack.Finalize.Add(
 						middleware.FinalizeMiddlewareFunc(
-							"DetachRolePolicyForApiErrorMock",
+							"DetachRolePolicyApiErrorMock",
 							func(context.Context, middleware.FinalizeInput, middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
 								return middleware.FinalizeOutput{
 									Result: &iam.DetachRolePolicyOutput{},
