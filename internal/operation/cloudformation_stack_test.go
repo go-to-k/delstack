de981591dce5037822251001b125c8c6b5fca9d1
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-02-04 00:34:54 +0900

    feat: #72 stack names case-insensitive in stack selection (#90)
    * feat: #72 stack names case-insensitive in stack selection
    
    * docs: #72 README for StackName Selection in a case-insensitive
    
    * chore: #72 add description for case-insensitive search

--- internal/operation/cloudformation_stack_test.go
+++ internal/operation/cloudformation_stack_test.go
@@ -1614,6 +1614,38 @@ func TestCloudFormationStackOperator_ListStacksFilteredByKeyword(t *testing.T) {
 			wantErr: false,
 		},
 		{
+			name: "list stacks filtered by lower keyword successfully",
+			args: args{
+				ctx:        ctx,
+				keyword:    "teststack",
+				clientMock: mock,
+			},
+			want: want{
+				filteredStacks: []string{
+					"TestStack1",
+					"TestStack2",
+				},
+				err: nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "list stacks filtered by upper keyword successfully",
+			args: args{
+				ctx:        ctx,
+				keyword:    "TESTSTACK",
+				clientMock: mock,
+			},
+			want: want{
+				filteredStacks: []string{
+					"TestStack1",
+					"TestStack2",
+				},
+				err: nil,
+			},
+			wantErr: false,
+		},
+		{
 			name: "list stacks filtered by keyword but empty keyword successfully",
 			args: args{
 				ctx:     ctx,
