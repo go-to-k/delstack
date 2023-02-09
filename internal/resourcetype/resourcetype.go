84cc290f1bb121ffa82e54e93da31a0c162059c5
K-Goto <kenta.goto.1228@gmail.com>
2023-02-10 00:03:12 +0900

    reafactor: const to pascal (#108)

--- internal/resourcetype/resourcetype.go
+++ internal/resourcetype/resourcetype.go
@@ -1,21 +1,21 @@
 package resourcetype
 
 const (
-	S3_BUCKET            = "AWS::S3::Bucket"
-	IAM_ROLE             = "AWS::IAM::Role"
-	ECR_REPOSITORY       = "AWS::ECR::Repository"
-	BACKUP_VAULT         = "AWS::Backup::BackupVault"
-	CLOUDFORMATION_STACK = "AWS::CloudFormation::Stack"
-	CUSTOM_RESOURCE      = "Custom::"
+	S3Bucket            = "AWS::S3::Bucket"
+	IamRole             = "AWS::IAM::Role"
+	EcrRepository       = "AWS::ECR::Repository"
+	BackupVault         = "AWS::Backup::BackupVault"
+	CloudformationStack = "AWS::CloudFormation::Stack"
+	CustomResource      = "Custom::"
 )
 
 func GetResourceTypes() []string {
 	return []string{
-		S3_BUCKET,
-		IAM_ROLE,
-		ECR_REPOSITORY,
-		BACKUP_VAULT,
-		CLOUDFORMATION_STACK,
-		CUSTOM_RESOURCE,
+		S3Bucket,
+		IamRole,
+		EcrRepository,
+		BackupVault,
+		CloudformationStack,
+		CustomResource,
 	}
 }
