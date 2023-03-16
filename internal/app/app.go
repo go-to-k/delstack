1b69866b7057053c89245857459a73af46ceb6fd
go-to-k <kenta.goto.1228@gmail.com>
2023-03-17 00:04:18 +0900

    chore: re-change messages again

--- internal/app/app.go
+++ internal/app/app.go
@@ -173,7 +173,7 @@ func (a *App) selectStackName(stackNames []string) string {
 	var stackName string
 
 	label := "Select StackName." + "\n" +
-		"Child nested stacks and XXX_IN_PROGRESS(e.g. ROLLBACK_IN_PROGRESS) status stacks are not displayed."
+		"Nested child stacks and XXX_IN_PROGRESS(e.g. ROLLBACK_IN_PROGRESS) status stacks are not displayed."
 
 	for {
 		stackName = io.GetSelection(label, stackNames)
