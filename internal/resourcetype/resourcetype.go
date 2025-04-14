58992c9e8221a8790331c7545412768740ae6ee3
go-to-k <24818752+go-to-k@users.noreply.github.com>
2025-04-14 20:59:07 +0900

    resourcetype

--- internal/resourcetype/resourcetype.go
+++ internal/resourcetype/resourcetype.go
@@ -3,6 +3,7 @@ package resourcetype
 const (
 	S3Bucket            = "AWS::S3::Bucket"
 	S3DirectoryBucket   = "AWS::S3Express::DirectoryBucket"
+	S3TableBucket       = "AWS::S3Tables::TableBucket"
 	IamGroup            = "AWS::IAM::Group"
 	EcrRepository       = "AWS::ECR::Repository"
 	BackupVault         = "AWS::Backup::BackupVault"
@@ -14,6 +15,7 @@ func GetResourceTypes() []string {
 	return []string{
 		S3Bucket,
 		S3DirectoryBucket,
+		S3TableBucket,
 		IamGroup,
 		EcrRepository,
 		BackupVault,
