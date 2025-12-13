7a21641e8e11d3f220d787fe5d83616aa3d545a7
go-to-k <24818752+go-to-k@users.noreply.github.com>
2025-12-13 18:49:44 +0900

    chore(deps): upgrade tablewriter to v1

--- internal/io/tablewriter.go
+++ internal/io/tablewriter.go
@@ -4,15 +4,35 @@ import (
 	"strings"
 
 	"github.com/olekukonko/tablewriter"
+	"github.com/olekukonko/tablewriter/tw"
 )
 
 func ToStringAsTableFormat(header []string, data [][]string) *string {
 	tableString := &strings.Builder{}
-	table := tablewriter.NewWriter(tableString)
-	table.SetHeader(header)
-	table.SetRowLine(true)
-	table.SetAutoWrapText(false)
-	table.AppendBulk(data)
+	table := tablewriter.NewTable(tableString,
+		tablewriter.WithRendition(
+			tw.Rendition{
+				Symbols: tw.NewSymbols(tw.StyleASCII),
+				Borders: tw.Border{
+					Top:    tw.On,
+					Bottom: tw.On,
+					Left:   tw.On,
+					Right:  tw.On,
+				},
+				Settings: tw.Settings{
+					Separators: tw.Separators{
+						BetweenRows: tw.On,
+					},
+					Lines: tw.Lines{
+						ShowHeaderLine: tw.On,
+					},
+				},
+			},
+		),
+	)
+
+	table.Header(header)
+	table.Bulk(data)
 	table.Render()
 
 	stringAsTableFormat := tableString.String()
