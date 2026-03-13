a908fa893c14fa6dfed756e72037e043e5a9ab36
Kenta Goto <24818752+go-to-k@users.noreply.github.com>
2026-03-13 20:12:37 +0900

    refactor: remove unnecessary vars for range (#572)

--- internal/operation/backup_vault.go
+++ internal/operation/backup_vault.go
@@ -37,7 +37,6 @@ func (o *BackupVaultOperator) DeleteResources(ctx context.Context) error {
 	sem := semaphore.NewWeighted(int64(runtime.NumCPU()))
 
 	for _, backupVault := range o.resources {
-		backupVault := backupVault
 		if err := sem.Acquire(ctx, 1); err != nil {
 			return err
 		}
